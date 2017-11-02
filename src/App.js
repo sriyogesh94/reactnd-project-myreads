a5896c3edaaf6334d276c5e5414afe750da10f18
unknown <sriyogesh94@gmail.com>
2017-11-02 12:43:08 +0530

    Refactor code into Book Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import SearchPage from './Search'
+import Book from './Book'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -36,26 +38,7 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchPage />
         ) : (
           <div className="list-books">
             <div className="list-books-title">
@@ -68,24 +51,7 @@ class BooksApp extends React.Component {
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                      {this.state.books.map((book) => (book.shelf === "currentlyReading" && (
-                      <li key={book.title}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                          <div className="book-shelf-changer">
-                            <select value={book.shelf} onChange={this.changeBookShelf.bind(this, book.id)}>
-                              <option value="none" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors}</div>
-                      </div>
-                      </li>
+                      <Book key={book.id} book={book} eventHandler={this.changeBookShelf.bind(this, book.id)}/>
                   )))}
                     </ol>
                   </div>
@@ -95,24 +61,7 @@ class BooksApp extends React.Component {
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                     {this.state.books.map((book) => (book.shelf === "wantToRead" && (
-                      <li key={book.title}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                          <div className="book-shelf-changer">
-                            <select value={book.shelf} onChange={this.changeBookShelf.bind(this, book.id)}>
-                              <option value="none" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors}</div>
-                      </div>
-                      </li>
+                      <Book key={book.id} book={book} eventHandler={this.changeBookShelf.bind(this, book.id)}/>
                   )))}
                     </ol>
                   </div>
@@ -122,24 +71,7 @@ class BooksApp extends React.Component {
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                     {this.state.books.map((book) => (book.shelf === "read" && (
-                      <li key={book.title}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                          <div className="book-shelf-changer">
-                            <select value={book.shelf} onChange={this.changeBookShelf.bind(this, book.id)}>
-                              <option value="none" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors}</div>
-                      </div>
-                      </li>
+                      <Book key={book.id} book={book} eventHandler={this.changeBookShelf.bind(this, book.id)}/>
                   )))}
                     </ol>
                   </div>
--- src/Book.js
+++ src/Book.js
@@ -2,26 +2,29 @@ import React from 'react'
 import './App.css'
 
 class Book extends React.Component {
-    render() {
-        return (
-        <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.title})` }}></div>
-              <div className="book-shelf-changer">
-                <select>
-                  <option value="none" disabled>Move to...</option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
-              </div>
-            </div>
-            <div className="book-title">{this.props.title}</div>
-            <div className="book-authors">{this.props.author}</div>
-        </div>
-        )
-    }
-}
 
-export default Book
+  render() {
+    return (
+      <li key={this.props.book.title}>
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`}}></div>
+          <div className="book-shelf-changer">
+            <select value={this.props.book.shelf} onChange={this.props.eventHandler}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{this.props.book.title}</div>
+        <div className="book-authors">{this.props.book.authors}</div>
+      </div>
+      </li>
+    )
+  }
+}
+ 
+export default Book;
